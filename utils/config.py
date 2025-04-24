3f86af409cf5f15e57ccbd1c454b2704a3f527d1
Kiran <84580493+kiransrik@users.noreply.github.com>
2025-04-24 17:14:38 +0100

    Add files via upload

---
+++ utils/config.py
@@ -0,0 +1,9 @@
+# Base URL of the application under test
+BASE_URL = "https://demo.guru99.com/popup.php"  
+# Timeouts (in seconds)
+IMPLICIT_WAIT = 10
+EXPLICIT_WAIT = 20
+
+SCREENSHOT_DIR = "screenshots/"
+REPORT_DIR = "reports/"
+VIDEO_DIR = "videos/"  
